b46c916568d1f254dde1a70e82a8eb4cf43394ee
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-12-29 15:45:51 -0300

    Mudar bg via JavaScript completo

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,57 +1,68 @@
 const introducao = document.querySelector('#introducao')
 
 
-const titulo = document.querySelector('h1#title')
-const paragrafo = document.querySelector('p#sub-tilte')
 
-function diablo4() {
-    titulo.innerText = "Retorne à escuridão com o game Diablo IV" 
-    paragrafo.innerText = "O retorno de Lilith traz uma era de escuridão e sofrimento"
+class MudarTextos {
+    constructor(titulo, sub_titulo, url, trailer){
+        this.titulo = titulo
+        this.sub_titulo = sub_titulo
+        this.url = url
+        this.trailer = trailer
+        this.renderText(this.titulo, this.sub_titulo)
+        this.renderImg(this.url)
+        this.renderTrailer(this.trailer)
+    }
+
+    renderText(titulo, sub_titulo) {
+        const title = document.querySelector('h1#title')
+        const paragrafo = document.querySelector('p#sub-tilte')
+
+        title.innerText = titulo
+        paragrafo.innerText = sub_titulo
+        return
+    }
+
+    renderImg(url) {
+        const bg = document.querySelector('#introducao')
+        bg.style.backgroundImage = `url("${url}")`
+        bg.style.transition = '.5s'
+    }
+
+    renderTrailer(src) {
+        document.querySelector('#imgPageVideo').src = src
+    }
 }
-function hearthstone() {
-    titulo.innerText = "Novo pacote de expansão de Hearthstone" 
-    paragrafo.innerText = "A Horda e a Aliança se encontraram no Vale Alterac para lutar"
-}
-function shadowlands() {
-    titulo.innerText = "Desbrave as Terras Sombrias em Shadowlands!" 
-    paragrafo.innerText = "O que jaz além do mundo que você conhece?"
-}
-function diabloImortal() {
-    titulo.innerText = "Um capitulo inédito na saga de diablo" 
-    paragrafo.innerText = "Enfrente males ancestrais e descubra o que aconteceu .."
-}
-function starCraft() {
-    titulo.innerText = "O supremo jogo de estratégia em tempo real" 
-    paragrafo.innerText = "Jogue do seu jeito"
-}
+
 
 const iconesBtn = document.querySelectorAll('.iconeBtn')
-iconesBtn.forEach(Element => {
 
+iconesBtn.forEach(Element => {
     Element.addEventListener('click', elementoClicado => {
         const varelementoClicado = elementoClicado.currentTarget
+
         iconesBtn.forEach(e => e.style.borderColor = '#ffffff')
         varelementoClicado.style.borderColor = 'red'
 
         switch (varelementoClicado.id) {
             case 'Diablo-4':
-                diablo4()
+                new MudarTextos("Retorne à escuridão com o game Diablo IV", "O retorno de Lilith traz uma era de escuridão e sofrimento", "./imagens/banner-hero/games/diablo-bg.png", "./imagens/banner-hero/games/diablo-animation-cover.png")
                 break
+
             case 'Hearthstone':
-                hearthstone()
+                new MudarTextos("Novo pacote de expansão de Hearthstone", "A Horda e a Aliança se encontraram no Vale Alterac para lutar", "./imagens/banner-hero/games/hearthstone-bg.png", "./imagens/banner-hero/games/hearthstone-animation-cover.png")
                 break
+
             case 'Shadowlands':
-                shadowlands()
+                new MudarTextos("Desbrave as Terras Sombrias em Shadowlands!" , "O que jaz além do mundo que você conhece?", "./imagens/banner-hero/games/wow-bg.png", "./imagens/banner-hero/games/wow-animation-cover.png")
                 break
+
             case 'Diablo-imortal':
-                diabloImortal()
+                new MudarTextos("Um capitulo inédito na saga de diablo", "Enfrente males ancestrais e descubra o que aconteceu ..", "./imagens/banner-hero/games/diablo-immortal-bg.png", "./imagens/banner-hero/games/diablo-immortal-animation-cover.png")
                 break
+
             case 'StarCraft':
-                starCraft()
+                new MudarTextos("O supremo jogo de estratégia em tempo real", "Jogue do seu jeito", "./imagens/banner-hero/games/starcraft-2-bg.png", "./imagens/banner-hero/games/starcraft-2-animation-cover.png")
                 break
         }
-
-        introducao.style.backgroundImage = "url()"
-        console.log(varelementoClicado.id)
     })
 })
